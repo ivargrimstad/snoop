b37008c7857250b93ac40141b2e940d65d651b34
Ivar Grimstad <ivar.grimstad@gmail.com>
2015-06-24 16:49:43 +0200

    fixed imports

--- snoop-service/src/main/java/eu/agilejava/snoop/api/ServicesResource.java
+++ snoop-service/src/main/java/eu/agilejava/snoop/api/ServicesResource.java
@@ -24,20 +24,15 @@
 package eu.agilejava.snoop.api;
 
 import eu.agilejava.snoop.SnoopClientRegistry;
-import eu.agilejava.snoop.SnoopConfig;
 import javax.ejb.EJB;
-import javax.validation.Valid;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 /**
