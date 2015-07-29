b9c138f30fc8cbdabb5d47023cf2867d247e4eeb
Ivar Grimstad <ivar.grimstad@gmail.com>
2015-07-29 14:51:38 +0200

    added docker packaging and changed name from snoophello to hello

--- snoop-examples/snoop-hello/src/main/java/eu/agilejava/snoop/ApplicationConfig.java
+++ snoop-examples/snoop-hello/src/main/java/eu/agilejava/snoop/ApplicationConfig.java
@@ -33,7 +33,7 @@ import javax.ws.rs.core.Application;
  *
  * @author Ivar Grimstad (ivar.grimstad@gmail.com)
  */
-@EnableSnoopClient(name = "snoophello")
+@EnableSnoopClient(name = "hello")
 @ApplicationPath("api")
 public class ApplicationConfig extends Application {
 
