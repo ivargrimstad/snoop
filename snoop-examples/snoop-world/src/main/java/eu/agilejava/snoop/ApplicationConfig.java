f5d4f1436e98a972d504f4127fbcbcdfc7089391
Ivar Grimstad <ivar.grimstad@gmail.com>
2015-07-30 12:27:13 +0200

    renamed word example service

--- snoop-examples/snoop-world/src/main/java/eu/agilejava/snoop/ApplicationConfig.java
+++ snoop-examples/snoop-world/src/main/java/eu/agilejava/snoop/ApplicationConfig.java
@@ -33,7 +33,7 @@ import javax.ws.rs.core.Application;
  *
  * @author Ivar Grimstad (ivar.grimstad@gmail.com)
  */
-@EnableSnoopClient(name = "snoopworld")
+@EnableSnoopClient(name = "world")
 @ApplicationPath("api")
 public class ApplicationConfig extends Application {
 
